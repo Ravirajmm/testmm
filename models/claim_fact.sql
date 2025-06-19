22cb1970f27d717f7123b85c48a0f4e527e23fde
Raviraj A <ravirajsa7@gmail.com>
2025-06-19 08:54:32 +0000

    add lineage

---
+++ models/claim_fact.sql
@@ -0,0 +1,15 @@
+SELECT
+    c.customer_id,
+    c.email,
+    p.policy_id,
+    p.policy_type,
+    p.premium_amount,
+    cl.claim_id,
+    cl.claim_status,
+    cl.claim_date,
+    cl.claim_amount
+FROM {{ ref('claims') }} cl
+JOIN {{ ref('policy') }} p
+  ON cl.policy_id = p.policy_id
+JOIN {{ ref('customer') }} c
+  ON p.customer_id = c.customer_id
